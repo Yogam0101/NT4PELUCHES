ce3927ef8a60d90280e311f920a1287ec2566fda
Yogamie :v <61318584+Yogam0101@users.noreply.github.com>
2023-08-25 11:27:50 -0500

    Add files via upload

---
+++ PELUCHES.py
@@ -0,0 +1,27 @@
+PELUCHES=["ROSHI", "CONEJITO", "AGUACATITO"]
+OPCION=0
+
+OPCION=0
+print ("PELUCHERÍA PELUCHITOOOOOS")
+print ("1. AGREGAR PRODUCTO A LA BODEGA")
+print ("2. VER PRODUCTOS EN BODEGA")
+print ("3. OBTENER VALOR DEL INVENTARIO")
+print ("4. VER PRODUCTOS MÁS VENDIDO")
+print ("5. SALÍ")
+print (". . . . . . . . .")
+while OPCION!=5:
+    print ("ESTOY REPITIENDO PTMAE")
+    OPCION=int(input("DIGITA UN NÚMERO:"))
+    if OPCION==1:
+        print ("USTED ESTÁ EN OPCIÓN 1")
+    elif OPCION==2:
+        print ("USTED ESTÁ EN LA OPCIÓN 2")
+    elif OPCION==3:
+        print ("USTED ESTÁ EN LA OPCIÓN 3")
+    elif OPCION==4:
+        print ("USTED ESTÁ EN LA OPCIÓN 4")
+    else:
+        print ("OPCIÓN INVÁLIDA")
+    elif OPCION==5:
+        print ("USTED ESTÁ EN LA OPCIÓN 5")
+    print ("SALÍ DEL CICLO")
